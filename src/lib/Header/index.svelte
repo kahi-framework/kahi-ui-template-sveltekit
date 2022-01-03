37fb5a8bb0719ccc4a54511f364ef498d669c9d8
novacbn <31122674+novacbn@users.noreply.github.com>
2022-01-03 00:42:38 -0500

    - Added `Popover` usage to `Header`, to demonstrate how to perform collasping navigation

--- src/lib/Header/index.svelte
+++ src/lib/Header/index.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { page } from '$app/stores';
-	import { Divider, Menu, Omni, Text } from '@kahi-ui/framework';
+	import { Box, Divider, Menu, Omni, Popover, Text } from '@kahi-ui/framework';
 </script>
 
 <Omni.Container palette="dark" margin_bottom="large">
@@ -11,10 +11,28 @@
 	</Omni.Header>
 
 	<Omni.Footer>
-		<Menu.Container orientation="horizontal">
+		<Menu.Container orientation="horizontal" hidden="mobile">
 			<Menu.Anchor href="/" active={$page.path === '/'}>Home</Menu.Anchor>
 			<Menu.Anchor href="/about" active={$page.path === '/about'}>About</Menu.Anchor>
 			<Menu.Anchor href="/todos" active={$page.path === '/todos'}>Todos</Menu.Anchor>
 		</Menu.Container>
+
+		<Popover.Container
+			logic_id="header-popover"
+			hidden={['tablet', 'desktop', 'widescreen']}
+			dismissible
+		>
+			<Popover.Button variation="clear">+</Popover.Button>
+
+			<Popover.Section alignment_x="left" spacing="small">
+				<Box palette="auto" elevation="high" padding="medium" shape="rounded">
+					<Menu.Container>
+						<Menu.Anchor href="/" active={$page.path === '/'}>Home</Menu.Anchor>
+						<Menu.Anchor href="/about" active={$page.path === '/about'}>About</Menu.Anchor>
+						<Menu.Anchor href="/todos" active={$page.path === '/todos'}>Todos</Menu.Anchor>
+					</Menu.Container>
+				</Box>
+			</Popover.Section>
+		</Popover.Container>
 	</Omni.Footer>
 </Omni.Container>
