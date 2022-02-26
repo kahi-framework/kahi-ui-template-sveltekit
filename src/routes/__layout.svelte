732c48b170cf9634edd4d685c9f4f4239580c821
NovacBN <novacbn@gmail.com>
2022-02-26 07:27:55 -0500

    Rebase onto Skeleton SvelteKit Template w/ TypeScript (#6)

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,32 +1,6 @@
 <script lang="ts">
-	import '@kahi-ui/framework/dist/kahi-ui.framework.css';
-	import { Anchor, Box, Spacer, Text } from '@kahi-ui/framework';
-
-	import Header from '$lib/Header/index.svelte';
+  import "@kahi-ui/framework/dist/kahi-ui.framework.css";
+  import "@kahi-ui/framework/dist/kahi-ui.theme.default.css";
 </script>
 
-<Header />
-
-<main>
-	<slot />
-</main>
-
-<Spacer />
-
-<Box margin_top="large" padding_x="medium" padding_y="huge">
-	<Text align="center">
-		visit <Anchor href="https://kit.svelte.dev" palette="accent">kit.svelte.dev</Anchor> to learn SvelteKit
-		<br />
-		visit <Anchor href="https://kahi-ui.nbn.dev" palette="accent">kahi-ui.nbn.dev</Anchor> to learn Kahi
-		UI
-	</Text>
-</Box>
-
-<style>
-	:global(#svelte) {
-		display: flex;
-		flex-direction: column;
-
-		min-height: 100vh;
-	}
-</style>
+<slot />
